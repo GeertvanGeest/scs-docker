c07b1185db067121182e84dca04879d8151ceac5
Geert van Geest <geert.vangeest@bioinformatics.unibe.ch>
2021-08-25 17:55:05 +0200

    updated deep learning python packages

--- deep_learning/rstudio/install_packages.R
+++ deep_learning/rstudio/install_packages.R
@@ -1,4 +1,9 @@
-install.packages(c("tensorflow", "keras", "BiocManager", "Matrix", "Rtsne", "rsvd", "RColorBrewer"))
+install.packages(c(  "tensorflow", "keras", "BiocManager", "Matrix", "Rtsne", "rsvd",
+                     "RColorBrewer", "umap", "reshape2"))
 
 # Bioconductor packages:
-BiocManager::install(c("SingleCellExperiment", "scater", "cowplot"))
+BiocManager::install(c( "SingleCellExperiment", "scater", "cowplot", "scran",
+                        "batchelor", "ComplexHeatmap", "tximeta", 
+                        "AnnotationDbi"))
+
+devtools::install_github('fmicompbio/swissknife')
